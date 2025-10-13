0eb15b0b41a173036bff36b78d88e797a1fcce6a
Alexander Moryan <alexmoryan@gmail.com>
2025-10-13 13:40:37 -0400

    adding in logic to configure lambda / iam

--- infra/terraform/min_terraform_cicd.tf
+++ infra/terraform/min_terraform_cicd.tf
@@ -59,3 +59,56 @@ variable "api_base" {}
 output "lambda_name" {
   value = aws_lambda_function.cicd_rollup.function_name
 }
+
+resource "aws_iam_role" "lambda_exec" {
+    name = "lambda-cicd-rollup-role"
+    assume_role_policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [{
+        Effect = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action = "sts:AssumeRole"
+      }]
+    })
+  }
+  
+resource "aws_iam_role_policy" "lambda_policy" {
+    name = "lambda-cicd-rollup-policy"
+    role = aws_iam_role.lambda_exec.id
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = [
+            "logs:*",
+            "s3:*",
+            "events:*",
+            "lambda:*",
+            "codecommit:*"
+          ]
+          Resource = "*"
+        }
+      ]
+    })
+  }
+  
+resource "aws_lambda_function" "cicd_rollup" {
+    function_name = "cicd-rollup"
+    handler       = "lambda_function.handler"
+    runtime       = "python3.12"
+    role          = aws_iam_role.lambda_exec.arn
+    memory_size   = 512
+    timeout       = 900
+  
+    filename         = "${path.module}/function.zip"
+    source_code_hash = filebase64sha256("${path.module}/function.zip")
+  
+environment {
+      variables = {
+        GITLAB_TOKEN  = var.gitlab_token
+        GIT_REMOTE_URL = var.git_remote_url
+        API_BASE       = var.api_base
+      }
+    }
+  }
